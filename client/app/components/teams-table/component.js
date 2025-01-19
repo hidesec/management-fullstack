cc1bf8da02154890962a3b788389f9c561b6d59e
hidesec <raihanhafis212@gmail.com>
2025-01-20 05:21:50 +0700

    feat: add new components and update existing files
    - Add new sidebar component (app/components/sidebar/component.hbs)
    - Add new teams-table component (app/components/teams-table/component.hbs, app/components/teams-table/component.js)
    - Add new top-bar component (app/components/top-bar/component.hbs)
    - Add new application controller (app/controllers/application.js)
    - Update index.html (app/index.html)
    - Update styles (app/styles/app.css)
    - Update application template (app/templates/application.hbs)
    - Update package dependencies (package-lock.json, package.json)

---
+++ client/app/components/teams-table/component.js
@@ -0,0 +1,22 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class TeamTableComponent extends Component {
+  @action
+  initializeTable(element) {
+    $(element).DataTable({
+      responsive: true,
+      dom: '<"top"lf>rt<"bottom"ip><"clear">',
+      language: {
+        lengthMenu: 'Show _MENU_ entries',
+        info: 'Showing _START_ to _END_ of _TOTAL_ entries',
+        paginate: {
+          first: 'First',
+          last: 'Last',
+          next: 'Next',
+          previous: 'Previous',
+        },
+      },
+    });
+  }
+}
